ec0a6157571f935923a6ddfedad7e93e5e21d5d7
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2025-05-16 16:05:03 +0300

    sucsess fetch data and start to view it in the ui screen

--- codexal-Task/src/layout/layout.jsx
+++ codexal-Task/src/layout/layout.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect } from 'react'
+
+
+
+
+
 
 
 
 const Layout = () => {
 
+
+
+    const getUsers= async () => {
+    
+        try {
+          const res = await axios.get("https://jsonplaceholder.typicode.com/users")
+    
+          if (res.data?.length > 0) {
+            // setPosts(res?.data);
+            // getPostComments();
+            // setLoader(false)
+
+            console.log(res.data);
+            
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      };
+
+
+
+
+
+
+useEffect(() => {
+    getUsers()
+  }, []);
 
 
   return (
