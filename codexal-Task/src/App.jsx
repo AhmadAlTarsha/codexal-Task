c6c45a2f3b63c716ac90b32b01507769405c52bc
AhmadALTarsha <ahmadwael.altarsha@gmail.com>
2025-05-16 17:26:37 +0300

    add loader

--- codexal-Task/src/App.jsx
+++ codexal-Task/src/App.jsx
@@ -1,18 +1,33 @@
-import { useState } from 'react'
-
-import viteLogo from '/vite.svg'
-import './App.css'
-import Layout from './layout/layout'
+import { useEffect, useState } from 'react';
+import './App.css';
+import Layout from './layout/layout';
+import Loader from './components/loader/loader'; 
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getUsers = async () => {
+    try {
+      const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+      setUsers(response.data);
+    } catch (error) {
+      console.error("حدث خطأ أثناء جلب البيانات", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <>
-    
-   <Layout/>
+      {loading ? <Loader /> : <Layout users={users} />}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- codexal-Task/src/components/loader/loader.jsx
+++ codexal-Task/src/components/loader/loader.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React from 'react';
+import './Loader.css'; 
 
 const Loader = () => {
-  return <div>Loader</div>;
+  return (
+    <div className="loader-container">
+      <div className="loader-spinner" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
 };
 
 export default Loader;
